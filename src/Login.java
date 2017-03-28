8725dfe49c43e8ff4a4e400ef32eb8d892a324e2
László Székely-Tóth <notisracer@gmail.com>
2017-03-28 15:29:56 +0200

    login post works

--- src/Login.java
+++ src/Login.java
@@ -5,10 +5,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Enumeration;
 
-@WebServlet(name = "Login")
 public class Login extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("test");
+        System.out.println(request.getParameter("email"));
+
+        if (request.getParameterMap().keySet().size() == 1
+                && request.getParameterMap().containsKey("email")) {
+
+        }
+
+
 
     }
 

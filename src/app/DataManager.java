f090bdad13a9603d6e627baf3ea53428037030dc
David Szilagyi <davidszilagyi@citromail.hu>
2017-03-30 14:32:21 +0200

    Print out users by role

--- src/app/DataManager.java
+++ src/app/DataManager.java
@@ -9,12 +9,27 @@ import java.util.List;
 public class DataManager {
     private List<User> userDatabase = new ArrayList<>();
 
+
     public void addUser(User user) {
         userDatabase.add(user);
     }
 
     public List<User> getUserDatabase() {
         return userDatabase;
+    }
+
+    public void printUsers(String role, ArrayList<User> database) {
+        if(role.equals("mentor")) {
+            for(User user: database) {
+                System.out.println(String.format("User email: %s Role: %s", user.getEmail(), user.getRole()));
+            }
+        } else {
+            for(User user: database) {
+                if(!user.getRole().equals("mentor")) {
+                    System.out.println(String.format("User email: %s Role: %s", user.getEmail(), user.getRole()));
+                }
+            }
+        }
     }
 
     public void setUserDatabase(CSVRW csvrw) {

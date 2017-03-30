13c862c0353cafaa95ea4f428b5f75ebc6d24278
László Székely-Tóth <notisracer@gmail.com>
2017-03-30 15:04:16 +0200

    Added getRole & getEmail, removed getUser

--- src/app/User.java
+++ src/app/User.java
@@ -1,7 +1,5 @@
 package app;
 
-import java.util.ArrayList;
-import java.io.*;
 public class User {
     private String username;
     private String emailAddress;
@@ -16,16 +14,23 @@ public class User {
         this.password = password;
 
     }
+
     @Override
-    public String toString(){
-        return this.username + ";" + this.emailAddress + ";" + this.role ;
+    public String toString() {
+        return this.username + ";" + this.emailAddress + ";" + this.role;
     }
 
     public String getPassword() {
         return password;
     }
 
-    public String getUsername(){return username;}
+    public String getRole() {
+        return role;
+    }
+
+    public String getEmail() {
+        return emailAddress;
+    }
 
 }
 

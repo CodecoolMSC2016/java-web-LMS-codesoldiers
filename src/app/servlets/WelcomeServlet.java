826b70e88b73dd6f1a156bb61e3da8d256ddf9c6
László Székely-Tóth <notisracer@gmail.com>
2017-03-30 11:53:18 +0200

    welcome redirects to login if not logged in

--- src/app/servlets/WelcomeServlet.java
+++ src/app/servlets/WelcomeServlet.java
@@ -8,12 +8,15 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 public class WelcomeServlet extends HttpServlet {
     PageWriter pageWriter = new PageWriter();
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
         //out.println("Welcome user");
@@ -24,7 +27,12 @@ public class WelcomeServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        RequestDispatcher welcome = request.getRequestDispatcher("welcome.html");
-        welcome.forward(request, response);
+        HttpSession session = request.getSession();
+        if (session.getAttribute("user") == null) {
+            response.sendRedirect("login");
+        } else {
+            RequestDispatcher welcome = request.getRequestDispatcher("welcome.html");
+            welcome.forward(request, response);
+        }
     }
 }

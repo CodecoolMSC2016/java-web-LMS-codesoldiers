f91bf8aca10b829a958083d6982ca0ca8bbe2f5c
László Székely-Tóth <notisracer@gmail.com>
2017-04-10 13:42:57 +0200

    Implemented delete and add page in curriculum servlet

--- src/app/servlets/CurriculumServlet.java
+++ src/app/servlets/CurriculumServlet.java
@@ -2,6 +2,7 @@ package app.servlets;
 
 import app.PageManager;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,41 +13,96 @@ import java.util.Map;
 public class CurriculumServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         PageManager pageManager = PageManager.getInstance();
+        Map<String, String[]> parameterMap = request.getParameterMap();
         String method = request.getParameter("method");
 
-        Map<String, String[]> parameterMap = request.getParameterMap();
+        int error = 500;
         switch (method) {
             case "delete":
-                delete(parameterMap);
+                error = delete(pageManager, parameterMap);
                 break;
             case "modify":
-                modify(parameterMap);
+                // error = modify(parameterMap);
+                error = 501;
                 break;
-            case "put":
-                put(parameterMap);
+            case "addTextPage":
+                // title, content
+                addTextPage(pageManager, parameterMap);
+                break;
+            case "addAssignmentPage":
+                // title, question, maxScore
+                addAssignmentPage(pageManager, parameterMap);
                 break;
             default:
-                response.sendError(404);
+                error = 404;
                 break;
+        }
+        if (error >= 400) {
+            response.sendError(error);
         }
     }
 
-    private void delete(Map<String, String[]> parameterMap) {
+    private int delete(PageManager pageManager, Map<String, String[]> parameterMap) {
         // required params: id
 
+        if (parameterMap.containsKey("id")) {
+            try {
+                int id = Integer.valueOf(parameterMap.get("id")[0]);
+                boolean successful = pageManager.removePageById(id);
+                if (successful) {
+                    return 200;
+                }
+            } catch (NumberFormatException e) {
+                return 400;
+            }
+        }
+        return 400;
     }
 
-    private void modify(Map<String, String[]> parameterMap) {
+    private int modifyPage(PageManager pageManager, Map<String, String[]> parameterMap) {
         // required params: id, modified property's name, modified value
-
+        return -1;
     }
 
-    private void put(Map<String, String[]> parameterMap) {
-        // required params: page type, title, [content] || [question, maxScore]
+    private int addTextPage(PageManager pageManager, Map<String, String[]> parameterMap) {
+        // required params: title, content
 
+        if (parameterMap.containsKey("title") &&
+                parameterMap.containsKey("content")) {
+            String title = parameterMap.get("title")[0];
+            String content = parameterMap.get("content")[0];
+            if (!title.equals("") || !content.equals("")) {
+                pageManager.addTextPage(title, content);
+                return 200;
+            }
+        }
+        return 400;
+    }
+
+    private int addAssignmentPage(PageManager pageManager, Map<String, String[]> parameterMap) {
+        // required params: title, question, maxScore
+
+        if (parameterMap.containsKey("title") &&
+                parameterMap.containsKey("question") &&
+                parameterMap.containsKey("maxScore")) {
+            try {
+                String title = parameterMap.get("title")[0];
+                String question = parameterMap.get("question")[0];
+                int maxScore = Integer.valueOf(parameterMap.get("maxScore")[0]);
+                if (!title.equals("") || !question.equals("")) {
+                    pageManager.addAssignmentPage(title, question, maxScore);
+                    return 200;
+                }
+            } catch (NumberFormatException e) {
+                return 400;
+            }
+        }
+        return 400;
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         PageManager pageManager = PageManager.getInstance();
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("curriculum.html");
+        requestDispatcher.forward(request, response);
     }
 }

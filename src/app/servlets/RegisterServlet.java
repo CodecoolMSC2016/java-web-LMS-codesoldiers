91428c103879d97b3b71e713c2a61b38b711d8f3
davidszilagyi <szilagyidav@gmail.com>
2017-04-25 11:23:59 +0200

    RegisterServlet completed

--- src/app/servlets/RegisterServlet.java
+++ src/app/servlets/RegisterServlet.java
@@ -1,7 +1,6 @@
 package app.servlets;
 
-import app.CSVRW;
-import app.User;
+import app.DatabaseManager;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -9,43 +8,33 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.List;
 
 public class RegisterServlet extends HttpServlet {
+    DatabaseManager dbm = DatabaseManager.getInstance();
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        if (checkInputs()) {
-            register(request.getParameter("user"),
+        if (checkInputs(request.getParameter("pass"))) {
+            dbm.addNewUser(request.getParameter("user"),
                     request.getParameter("email"),
                     request.getParameter("role"),
                     request.getParameter("pass"));
         }
         response.sendRedirect("login");
+        PrintWriter out = response.getWriter();
+        out.println("<p style='margin-left: 250'>Successful registration!</p>");
     }
 
-    private void register(String user, String email, String role, String password) {
-        // TODO: register into SQL
-        CSVRW io = new CSVRW("userdatabase.csv");
-        // list from readcsv
-        List<User> userList = io.readUserDatabase();
-        System.out.println(password);
-        // new user from params
-        User newUser = new User(user, email, role, password);
-
-        // add user to readcsv list
-        userList.add(newUser);
-        // saveto csv
-        io.saveToCSV(userList);
-        System.out.println(userList.size());
-        System.out.println("csv saved");
-    }
-
-    private boolean checkInputs() {
+    public boolean checkInputs(String string) {
+        for (int i = 0; i < string.length(); i++) {
+            if (!Character.isLetterOrDigit(string.charAt(i))) {
+                return false;
+            }
+        }
         return true;
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.sendRedirect("/login");
+        response.sendRedirect("login");
     }
 }

e9f0431342b9a15ede28c535980a7ed2234c3e13
ThomasKenyeres <kenyerest97@gmail.com>
2017-04-26 14:33:35 +0200

    cardcreator style changed

---
+++ web/cardcreator.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $('input[type="radio"]').click(function () {
+        var inputValue = $(this).attr("value");
+        var targetBox = $("." + inputValue);
+        $(".box").not(targetBox).hide();
+        $(targetBox).show();
+    });
+});
+
+function radioChanged() {
+    if(document.getElementsByName('pageType').checked) {
+        $("#txtAge").show();
+    } else {
+        $("#txtAge").hide();
+    }
+
+}
